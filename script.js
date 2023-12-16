509fe87a4980d1fca4cff7b857ef5ef911c470e0
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-16 18:37:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 const parseCode = (str) => {
   // your code here
-
-	const regex = /^(.*?)000(.*?)000(\d+)$/;
-  
-	const match = input.match(regex);
-
-	if (match) {
-		const [, firstName, lastName, id] = match;
-	    return { firstName, lastName, id };
-	} else {
-	    return null;
+let f_Name="";
+	let l_name="";
+	let id="";
+	let i=0;
+	while(str.charAt(i)!=="0"){
+		f_Name+=str.charAt(i);
+		i++;
 	}
+	while(str.charAt(i)==="0"){
+		i++;
+	}
+	while(str.charAt(i)!=="0"){
+		l_name+=str.charAt(i);
+		i++;
+	}
+	while(str.charAt(i)==="0"){
+		i++;
+	}
+	while(i<str.length && str.charAt(i)!=="0"){
+		id+=str.charAt(i);
+		i++;
+	}
+	return {
+		firstName: f_Name,
+		lastName: l_name,
+		id: id
+	}
+	
 };
 
 // Do not change the code below
