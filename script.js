cfb85dc56a0b6f755e5bd6a89d7269e0e6f52b1b
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-16 18:33:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const parseCode = (str) => {
   // your code here
+
+	const regex = /^(.*?)000(.*?)000(\d+)$/;
+  
+	const match = input.match(regex);
+
+	if (match) {
+		const [, firstName, lastName, id] = match;
+	    return { firstName, lastName, id };
+	} else {
+	    return null;
+	}
 };
 
 // Do not change the code below
